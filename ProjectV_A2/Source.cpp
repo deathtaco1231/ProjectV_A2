e0116891e70365963b80aeba8b69c05be8606ed1
Julian Cumming <jcumming@conestogac.on.ca>
2024-09-19 22:01:15 -0400

    - Updated Source.cpp for Step #3 - Added Two Text Files Needed For Assignment

--- ProjectV_A2/Source.cpp
+++ ProjectV_A2/Source.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-int main(void) {
+struct STUDENT_DATA {
+	string FirstName;
+	string LastName;
+};
 
+int main(void) {
+	std::ifstream studentDataFile; 
+	studentDataFile.open("StudentData.txt");
+	std::string line;
+	std::vector<STUDENT_DATA> studentDataVector;
+	while (!studentDataFile.eof()) {
+		getline(studentDataFile, line);
+		std::stringstream curLine(line);
+		STUDENT_DATA curStudent;
+		getline(curLine, curStudent.FirstName, ',');
+		getline(curLine, curStudent.LastName);
+		studentDataVector.push_back(curStudent);
+	}
 
 
 	return 1;
